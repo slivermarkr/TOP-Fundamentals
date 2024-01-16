7f7de18b256c778b8bbb779124616800ba3302c3
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-16 13:29:45 +0800

    some rgb

--- dom-crash-course/script.js
+++ dom-crash-course/script.js
@@ -1,63 +1,87 @@
-// //console.dir(document);
-// /*
-// console.log(document.head)
-// console.log(document.body);
-// let items = document.getElementsByClassName('list-group-item');
+// // //console.dir(document);
+// // /*
+// // console.log(document.head)
+// // console.log(document.body);
+// // let items = document.getElementsByClassName('list-group-item');
 
-// console.log(items);
-// items[0].style.backgroundColor = 'yellow';
-// items[1].style.backgroundColor = 'aqua';
+// // console.log(items);
+// // items[0].style.backgroundColor = 'yellow';
+// // items[1].style.backgroundColor = 'aqua';
 
-// for(let item of items){
-//     item.style.fontWeight = 'bold';
-// }*/
-// //let li = document.getElementsByTagName('li');
-// //console.log(li)
-// //console.log(li[0]);
-// // let odd = document.querySelectorAll('li:nth-child(odd)');
-// // let even = document.querySelectorAll('li:nth-child(even)');
-
-
-// // for(let i = 0; i < odd.length; i++){
-// //     odd[i].style.backgroundColor = '#f4f4f4';
-// //     even[i].style.backgroundColor = '#ccc' ;
-// // }
+// // for(let item of items){
+// //     item.style.fontWeight = 'bold';
+// // }*/
+// // //let li = document.getElementsByTagName('li');
+// // //console.log(li)
+// // //console.log(li[0]);
+// // // let odd = document.querySelectorAll('li:nth-child(odd)');
+// // // let even = document.querySelectorAll('li:nth-child(even)');
 
 
-// let items = document.querySelector('#items');
-// // // 
-// // console.log(items.children);
-// // items.children[1].style.backgroundColor = 'yellow';
-// // // items.firstElemendChild.style.backgroundColor = 'red';
-
-// // console.log(items.previousElementSibling);
-// // console.log(items.nextElementSibling);
-
-// // items.previousElementSibling.style.backgroundColor = 'red';
-// const header = document.querySelector('header .container');
-// const h1 = document.querySelector('header h1');
-
-// let div = document.createElement('div');
-// div.className = 'cont';
-// div.id = "hello";
-
-// div.setAttribute('title','hihello');
-
-// let divText = document.createTextNode("Hello World");
+// // // for(let i = 0; i < odd.length; i++){
+// // //     odd[i].style.backgroundColor = '#f4f4f4';
+// // //     even[i].style.backgroundColor = '#ccc' ;
+// // // }
 
 
+// // let items = document.querySelector('#items');
+// // // // 
+// // // console.log(items.children);
+// // // items.children[1].style.backgroundColor = 'yellow';
+// // // // items.firstElemendChild.style.backgroundColor = 'red';
 
-// div.appendChild(divText);
-// header.insertBefore(div,h1);
+// // // console.log(items.previousElementSibling);
+// // // console.log(items.nextElementSibling);
 
-// div.style.fontSize = '30px'
-// console.log(div);
-const button = document.querySelector('#button');
+// // // items.previousElementSibling.style.backgroundColor = 'red';
+// // const header = document.querySelector('header .container');
+// // const h1 = document.querySelector('header h1');
 
-button.addEventListener('click',clicked);
+// // let div = document.createElement('div');
+// // div.className = 'cont';
+// // div.id = "hello";
+
+// // div.setAttribute('title','hihello');
+
+// // let divText = document.createTextNode("Hello World");
 
 
-function clicked(){
-    let header = document.querySelector('#header-title');
-    header.textContent = "Hello Mom";
+
+// // div.appendChild(divText);
+// // header.insertBefore(div,h1);
+
+// // div.style.fontSize = '30px'
+// // console.log(div);
+// const button = document.querySelector('#button');
+
+// button.addEventListener('click',(e) => {
+//    console.log(e.altKey);
+//    console.log(e.ctrlKey);
+//    console.log(e.shiftKey);
+// });
+
+
+const button = document.querySelector("#button");
+let box = document.querySelector("#box");
+// button.addEventListener('click',runEvent);
+// // button.addEventListener('dblclick',runEvent);
+// box.addEventListener('mouseenter',runEvent);
+// box.addEventListener('mouseleave',runEvent);
+
+// box.addEventListener('mouseover',runEvent);
+// box.addEventListener('mouseout',runEvent);
+
+
+let output = document.querySelector(".output");
+
+box.addEventListener('mousemove',runEvent)
+
+function runEvent(e){
+ console.log(`EVENT TYPE: `+e.type);
+
+ let xoff = e.offsetX;
+ let yoff = e.offsetY;
+
+ output.innerHTML = `<h3>MouseX: ${xoff} </h3> <h3>MouseY: ${yoff}`; 
+ box.style.backgroundColor = `rgb(${xoff},${yoff},40)`;
 }
